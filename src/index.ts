d6b502378cf798217e65cc59daa05446bc66c301
김정진 <i31pc17@gmail.com>
2022-08-09 17:42:21 +0900

    insertAllQuery 빈값 예외처리

--- src/index.ts
+++ src/index.ts
@@ -1134,6 +1134,8 @@ class DBQB {
                         return null;
                     }
                     data[field] = chkVal;
+                } else {
+                    data[field] = '';
                 }
                 sData += `, "${data[field]}"`;
             }
